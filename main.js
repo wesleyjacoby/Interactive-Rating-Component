9c20ff45c99edcf847ce3cc55131164bf73471af
Wesley Jacoby <wesleyjacoby@gmail.com>
2022-08-24 13:30:52 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#rating-form');
+const radioButtons = document.querySelectorAll('input[name="rating"]');
+const rating = document.querySelector('span');
+
+const mainContainer = document.querySelector('.main-container');
+const innerContainer = document.querySelector('.inner-container');
+const innerContainer2 = document.querySelector('.inner-container2');
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    for (button of radioButtons) {
+        if (button.checked) {
+            console.log(button.value);
+            rating.append(button.value);
+            mainContainer.classList.add('fade-in');
+            innerContainer.classList.add('hide');
+            innerContainer2.classList.remove('hide');
+        }
+    }
+
+})
